fb27ae60c2071dc48216f34b14114a060ef65be0
Bulent Gorkem <bgorkem@hotmail.com>
2019-10-15 20:40:43 +0100

    Implement using React hooks

--- src/index.js
+++ src/index.js
@@ -1,57 +1,49 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
-import { messageService } from "./messageService";
+import { messageService } from './messageService';
 
-import "./styles.css";
+import './styles.css';
 
-class App extends React.Component {
-  state = {
-    messages: []
-  };
+const sendMessage = () => {
+  messageService.sendMessage(`${Date.now()}`);
+};
 
-  componentDidMount() {
-    // subscribe to home component messages
-    this.subscription = messageService.getMessage().subscribe(message => {
+const clearMessages = () => {
+  messageService.clearMessages();
+};
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const subscription = messageService.getMessage().subscribe(message => {
       if (message) {
-        // add message to local state if not empty
-        this.setState({ messages: [...this.state.messages, message] });
+        setMessages([...messages, message]);
       } else {
-        // clear messages when empty message received
-        this.setState({ messages: [] });
+        setMessages([]);
       }
+
+      return () => {
+        subscription.dispose();
+      };
     });
-  }
+  });
 
-  componentWillUnmount() {
-    this.subscription.dispose();
-  }
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={sendMessage}>Send Message</button>
+      <button onClick={clearMessages}>Clear Message</button>
+      {messages.map((message, index) => (
+        <div key={index} className="alert alert-success">
+          {message.text}
+        </div>
+      ))}
+    </div>
+  );
+};
 
-  sendMessage(e) {
-    messageService.sendMessage(`${Date.now()}`);
-  }
-
-  clearMessages(e) {
-    messageService.clearMessages();
-  }
-
-  render() {
-    const { messages } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.sendMessage}>Send Message</button>
-        <button onClick={this.clearMessages}>Clear Message</button>
-        {messages.map((message, index) => (
-          <div key={index} className="alert alert-success">
-            {message.text}
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
